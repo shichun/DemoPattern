c1862de6c2e4cfe1baeb7b7e43e3f3c0945292be
shichun <yaoshichuny@aliyun.com>
2015-11-27 09:54:52 +0800

    test pattern

--- src/com/peter/pattern/AbstractFactory.java
+++ src/com/peter/pattern/AbstractFactory.java
@@ -1,5 +1,23 @@
 package com.peter.pattern;
 
-public class AbstractFactory {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class AbstractFactory {
+    public static void main(String[] args) {
+        String input = "User clientId=23421. Some more text clientId=33432. This clientNum=100";
+
+        Pattern p = Pattern.compile("(clientId=)(\\d+)");
+        Matcher m = p.matcher(input);
+
+        StringBuffer result = new StringBuffer();
+        while (m.find()) {
+            System.out.println(m.group(1));
+            System.out.println("Masking: " + m.group(2));
+            System.out.println(result);
+            m.appendReplacement(result, m.group(1) + "***masked***");
+        }
+        m.appendTail(result);
+        System.out.println(result);
+    }
 }
